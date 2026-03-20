03fdadffb1fcc9f38ab9e71dba22b8c203b5e758
godxmodsnewsm-cyber <godxmodsnewsm@gmail.com>
2026-03-20 04:04:31 +0000

    fix api

--- api/key.js
+++ api/key.js
@@ -1,12 +1,6 @@
 export default function handler(req, res) {
-      res.status(200).json({
-<<<<<<< HEAD
-          message: "API working 🔥",
-              key: Math.random().toString(36).substring(2, 10)
-                });
-                }
-=======
-          key: "my-secret-key"
-            });
-            }
->>>>>>> eb61e15 (fix api)
+    res.status(200).json({
+        key: "my-secret-key"
+          });
+          }
+
