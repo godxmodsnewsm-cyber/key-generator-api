e3800ceb897704e037cd47d58f74f68d3e92b589
godxmodsnewsm-cyber <godxmodsnewsm@gmail.com>
2026-03-21 02:23:23 +0000

    final setup

--- api/verify.js
+++ api/verify.js
@@ -46,4 +46,4 @@ export default async function handler(req, res) {
                                                                                     rng: Math.floor(Date.now() / 1000)
                                                                                         }
                                                                                           })
-                                                                                          }                                                         }
+                                                                                          }                                                                                    }                                                         }
